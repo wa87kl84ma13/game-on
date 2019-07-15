e3a3d195b472c0469b904a31f5fe55e90fe2b6aa
wa87kl84ma13 <51495821+wa87kl84ma13@users.noreply.github.com>
2019-07-15 22:31:44 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ hamburgerMenu.addEventListener('click', function() {
 });
 
 closeNav.addEventListener('click', function() {
-    mainNav.style.display = 'none';
+        mainNav.style.display = 'none';
 });
 
 // Create game image gallery
@@ -24,3 +24,51 @@ function selectedImage(e) {
 }
 
 imageGallery.forEach(img => img.addEventListener('click', selectedImage));
+
+// Close modal
+const closeModalBox = document.querySelector('.close-modal-sign-up');
+
+function closeModal() {
+    signUpModal.style.display = 'none';
+}
+
+closeModalBox.addEventListener('click', closeModal);
+
+// Sign up modal
+const signUpModal = document.querySelector('#modal');
+const signUp = document.querySelector('#sign-up');
+const thankYouModal = document.querySelector('#thank-you-modal');
+
+function loadModal() {
+    signUpModal.style.display = 'block';
+    if (window.matchMedia("(max-width: 480px)").matches) {
+         mainNav.style.display = 'none';
+    } else {
+        console.log('error');
+    }
+}
+
+signUp.addEventListener('click', loadModal);
+
+// Form validation
+const signUpForm = document.querySelector('#form');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const errorMessage = document.querySelector('#error');
+const success = document.querySelector('#success');
+
+function validateForm(e) {
+    if(email.value === '' || password.value === '') {
+        errorMessage.innerHTML = 'Please fill in the fields';
+        errorMessage.classList.add('error-message');
+        setTimeout(() => errorMessage.remove(), 4000);
+    } else {
+        thankYouModal.style.display = 'block';
+        signUpModal.style.display = 'none';
+        setTimeout(() => thankYouModal.remove(), 6000);
+    }
+
+    e.preventDefault();
+}
+
+signUpForm.addEventListener('submit', validateForm);
