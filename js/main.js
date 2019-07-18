ecb4eb1c599fcdae23b1870740cf12e25edf0196
wa87kl84ma13 <51495821+wa87kl84ma13@users.noreply.github.com>
2019-07-18 21:31:48 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const hamburgerMenu = document.querySelector('.hamburger-menu');
 const mainNav = document.querySelector('.main-nav');
 const closeNav = document.querySelector('.close');
 
+hamburgerMenu.onclick = function () {}
+
 hamburgerMenu.addEventListener('click touchstart', function() {
     mainNav.style.display = 'block';
 });
