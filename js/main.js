3a3bfeecbcd721d14c860b151fc6d204d2c56458
wa87kl84ma13 <51495821+wa87kl84ma13@users.noreply.github.com>
2019-07-24 21:29:46 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,75 +24,19 @@ $('.close').bind('click touchstart', function() {
 
 
 // Create game image gallery
-const currentImage = document.querySelector('#current');
-const imageGallery = document.querySelectorAll('.review-gallery-image');
+const currentImage = document.querySelector('#large-image');
+const imageGallery = document.querySelectorAll('.small-image');
 
 function selectedImage(e) {
     currentImage.src = e.target.src;
 
     currentImage.classList.add('fade-in');
 
-    setTimeout(() => currentImage.classList.remove('fade-in'), 500);
-}
-
-imageGallery.forEach(img => img.addEventListener('click', selectedImage));
-
-//JQUERY CHANGE IMG SRC ON CLICK
-/*
-$('.small-image').bind('click touchstart', function() {
-    var largeImage = $(this).attr('src');
-    $('#large-image').attr('src',largeImage);
-    $('#large-image').addClass('fade-in');
     setTimeout(function() {
-        document.querySelector('#large-image').classList.remove('fade-in')
-    }, 500);
+        currentImage.classList.remove('fade-in')
+    }, 4000);
+}
+
+imageGallery.forEach(function(img) {
+    img.addEventListener('click', selectedImage);
 });
-*/
-
-// Close modal
-const closeModalBox = document.querySelector('.close-modal-sign-up');
-
-function closeModal() {
-    signUpModal.style.display = 'none';
-}
-
-closeModalBox.addEventListener('click', closeModal);
-
-// Sign up modal
-const signUpModal = document.querySelector('#modal');
-const signUp = document.querySelector('#sign-up');
-const thankYouModal = document.querySelector('#thank-you-modal');
-
-function loadModal() {
-    signUpModal.style.display = 'block';
-    if (window.matchMedia("(min-width: 320px) and (max-width: 768px)").matches) {
-         mainNav.style.display = 'none';
-    } else {
-        console.log('error');
-    }
-}
-
-signUp.addEventListener('click', loadModal);
-
-// Form validation
-const signUpForm = document.querySelector('#form');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
-const errorMessage = document.querySelector('#error');
-const success = document.querySelector('#success');
-
-function validateForm(e) {
-    if(email.value === '' || password.value === '') {
-        errorMessage.innerHTML = 'Please fill in the fields';
-        errorMessage.classList.add('error-message');
-        setTimeout(() => errorMessage.remove(), 4000);
-    } else {
-        thankYouModal.style.display = 'block';
-        signUpModal.style.display = 'none';
-        setTimeout(() => thankYouModal.remove(), 6000);
-    }
-
-    e.preventDefault();
-}
-
-signUpForm.addEventListener('submit', validateForm);
